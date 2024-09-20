a88a2cee3c88fd9ca1217c18396aef24da5c7395
Grivin.NA <neuronspectrelin@gmail.com>
2024-09-21 02:01:17 +0300

    linking checker

---
+++ tests/basic/installment_check.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "db_field.hpp"
+#include "handbook.hpp"
+int main(){
+    std::cerr << "ALL PACKAGES INSTALLED SUCCESSFULLY\n";
+    std::cerr << "LINKS GOT CREATED CORRECTLY\n";
+    return 0;
+}
