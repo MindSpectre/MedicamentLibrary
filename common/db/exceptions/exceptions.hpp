3cf330260458757c4706b536d275d50ea129b9cb
Grivin.NA <neuronspectrelin@gmail.com>
2024-09-24 23:39:37 +0300

    adjust gitignore add error_codes.hpp

--- common/db/exceptions/exceptions.hpp
+++ common/db/exceptions/exceptions.hpp
@@ -3,41 +3,58 @@
 #include <stdexcept>
 #include <string>
 
+#include "error_codes.hpp"
+
 namespace drug_lib::common::database::exceptions
 {
     class DatabaseException : public std::runtime_error
     {
+    protected:
+        errors::DbErrorCode m_error_code = errors::DbErrorCode::UNKNOWN_ERROR;
+
     public:
-        explicit DatabaseException(const std::string& message)
-            : std::runtime_error(message) {}
+        explicit DatabaseException(const std::string& message, const errors::DbErrorCode error_code) :
+            std::runtime_error(message), m_error_code(error_code)
+        {
+        }
+
+        [[nodiscard]] errors::DbErrorCode get_error() const { return m_error_code; }
     };
 
-    class ConnectionException : public DatabaseException
+    class ConnectionException final : public DatabaseException
     {
     public:
-        explicit ConnectionException(const std::string& message)
-            : DatabaseException("ConnectionException: " + message) {}
+        explicit ConnectionException(const std::string& message, const errors::DbErrorCode code)
+            : DatabaseException("ConnectionException: " + message, code)
+        {
+        }
     };
 
-    class QueryException : public DatabaseException
+    class QueryException final : public DatabaseException
     {
     public:
-        explicit QueryException(const std::string& message)
-            : DatabaseException("QueryException: " + message) {}
+        explicit QueryException(const std::string& message, const errors::DbErrorCode code)
+            : DatabaseException("QueryException: " + message, code)
+        {
+        }
     };
 
-    class TransactionException : public DatabaseException
+    class TransactionException final : public DatabaseException
     {
     public:
-        explicit TransactionException(const std::string& message)
-            : DatabaseException("TransactionException: " + message) {}
+        explicit TransactionException(const std::string& message, const errors::DbErrorCode code)
+            : DatabaseException("TransactionException: " + message, code)
+        {
+        }
     };
 
-    class InvalidIdentifierException : public DatabaseException
+    class InvalidIdentifierException final : public DatabaseException
     {
     public:
-        explicit InvalidIdentifierException(const std::string& message)
-            : DatabaseException("InvalidIdentifierException: " + message) {}
+        explicit InvalidIdentifierException(const std::string& message, const errors::DbErrorCode code)
+            : DatabaseException("InvalidIdentifierException: " + message, code)
+        {
+        }
     };
 
     // Additional exceptions can be added here
